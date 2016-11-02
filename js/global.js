d546e09636b2de683a07c8995f5dead3e8f8f548
fanuel nunes <fanuelson@gmail.com>
2016-11-02 12:34:54 -0200

    Remoção de código não usado em global.js

--- js/global.js
+++ js/global.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
-	/* MENU */
-	
-		$('.ui.sidebar')
-		  .sidebar({
-		    context: $('.bottom.segment')
-		  }).sidebar('attach events', ' #menuOpener');
-	
-		 $('#menuOpener').click(function(){
-			$('.pusher').removeClass('dimmed');
-		 });
-		 $('.pusher').removeClass('dimmed');
-
-	/* END MENU */
 		 
-	
-		
+
+
 });
